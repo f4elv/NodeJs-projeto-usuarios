01519cf8f45983e232dff16962d6a88942f587e8
Rafael Vasconcelos Fernandes <efael1986@gmail.com>
2025-07-27 15:19:35 -0400

    Implementação da rota de login com autenticação de token e senha criptografada

--- src/controllers/usuariosControllers.js
+++ src/controllers/usuariosControllers.js
@@ -1,20 +1,55 @@
 import prisma from '../prisma/client.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 
 export const criarUsuario = async (req, res) => {
-    const { nome, email, idade } = req.body;
+    const { nome, email, idade, senha } = req.body;
     
     try {
+        const senhaHash = await bcrypt.hash(senha, 10);
+
         const usuario = await prisma.usuario.create({
         data: {
             nome,
             email,
             idade,
+            senha: senhaHash, // Armazenando a senha criptografada
         },
         });
         res.status(201).json(usuario);
     } catch (error) {
         console.error('Erro ao criar usuário:', error);
         res.status(500).json({ erro: 'Erro ao criar usuário' }); 
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, senha } = req.body;
+
+    try {
+        const usuario = await prisma.usuario.findUnique({
+            where: { email },
+        });
+
+        if (!usuario) {
+            return res.status(404).json({ error: 'Usuário não encontrado' });
+        }
+
+        const senhaValida = await bcrypt.compare(senha, usuario.senha);
+        if (!senhaValida) {
+            return res.status(401).json({ error: 'Senha incorreta' });
+        }
+
+        const token = jwt.sign({ id: usuario.id }, process.env.JWT_SECRET, {
+            expiresIn: '2h', // Expira em 2 horas
+        });
+
+        res.status(200).json({ message: 'Login bem-sucedido', token });
+
+    } catch (error) {
+        console.error('Erro no login:', error);
+        res.status(500).json({ error: 'Erro ao realizar login' });
     }
 }
 
--- src/routes/usuariosRoute.js
+++ src/routes/usuariosRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { criarUsuario, listarUsuarios } from '../controllers/usuariosControllers.js';
+import { criarUsuario, listarUsuarios, login } from '../controllers/usuariosControllers.js';
 
 const router = express.Router();
 
-router.post('/usuarios', criarUsuario);
-router.get('/usuarios', listarUsuarios);
+router.post('/usuarios', criarUsuario);// Adicionando rota para criar usuário
+router.post('/usuarios/login', login); // Adicionando rota de login
+router.get('/usuarios', listarUsuarios);// Adicionando rota para listar usuários
+
 
 export default router;
